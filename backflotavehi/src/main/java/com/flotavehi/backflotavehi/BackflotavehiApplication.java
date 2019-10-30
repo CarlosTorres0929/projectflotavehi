6b87302ca141b5f98efac7cdff06676ee0b358be
503 <503@academicos.cesde.edu.co>
2019-10-30 18:50:14 -0500

    Conectando con javaweb

---
+++ backflotavehi/src/main/java/com/flotavehi/backflotavehi/BackflotavehiApplication.java
@@ -0,0 +1,13 @@
+package com.flotavehi.backflotavehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackflotavehiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackflotavehiApplication.class, args);
+	}
+
+}
---
+++ backflotavehi/src/test/java/com/flotavehi/backflotavehi/BackflotavehiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flotavehi.backflotavehi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackflotavehiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
